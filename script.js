20c3e74bdd59ea49379c7c5051298100988b0056
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-08-13 23:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function firstChar(text) {
   // your code here
+	if(text === "" || text === " "){
+		return '';
+	}
 	const char = text.slice(0,1);
 	return char;
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+//const text = prompt("Enter text:");
+//alert(firstChar(text));
